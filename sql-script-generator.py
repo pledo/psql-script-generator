5087b8e10d3c0d438a3825f0a00ce3ee68630ee5
Paulo Ledo <paulofledo@gmail.com>
2024-04-22 17:18:48 -0300

    Updating Readme.md

--- sql-script-generator.py
+++ sql-script-generator.py
@@ -5,18 +5,15 @@ import os
 import sys
 from jinja2 import Template
 
-def generate_sql_script(app_database, app_readwrite_role, app_user_role, app_user_pass, template_file, output_file):
+def generate_sql_script(user_args):
 
     # Load Jinja2 template from a file
-    template_path = os.path.abspath(os.path.join(os.path.dirname(__file__), template_file))
+    template_path = os.path.abspath(os.path.join(os.path.dirname(__file__), user_args.template))
 
     if not os.path.exists(template_path):
-        # This message works good when running the script from the command line
-        #print(error_message.format(template_file, os.getcwd()))
-        
-        # This message works good when running the script from a container
         error_message = "Error: Template file {}' not found"
-        print(error_message.format(template_file))
+        print(error_message.format(user_args.template, os.getcwd()))
+    
         exit(1)
 
     with open(template_path, 'r') as f:
@@ -24,31 +21,27 @@ def generate_sql_script(app_database, app_readwrite_role, app_user_role, app_use
 
     # Render the template    
     rendered_sql_script = template.render(
-        app_database=app_database,
-        app_readwrite_role=app_readwrite_role,
-        app_user_role=app_user_role,
-        app_user_pass=app_user_pass,
+        app_database=user_args.database,
+        app_readwrite_role=user_args.readwrite_role,
+        app_user=user_args.user,
+        app_user_pass=user_args.user_pass,
 
     )
 
     # Write the rendered SQL script to the output file
     
     # Error handling, if the output file exists, warn the user and stop the script
-    if os.path.exists(output_file):
-        error_message = "Error: The output file already exists; please choose another name or delete the existing one: {}" 
-        
-        # This message works good when running the script from the command line
-        #print(error_message.format(os.path.abspath(output_file)))
-
-        # This message works good when running the script from a container
-        print(error_message.format(output_file))
+    if os.path.exists(user_args.output_file):
+        error_message = "Error: The output file already exists; please choose another name or delete the existing one: {}"       
+        print(error_message.format(os.path.abspath(user_args.output_file)))
         exit(1)
+
     else:
-        output_filename = output_file
+        output_filename = user_args.output_file
         with open(output_filename, 'w') as f:
            f.write(rendered_sql_script)
 
-    print("SQL script generated successfully: {}".format(output_file))    
+    print("SQL script generated successfully: {}".format(user_args.output_file))    
     
 def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -58,13 +51,24 @@ def main():
     parser = argparse.ArgumentParser(description=usage_message)
     parser.add_argument('-d', '--database', required=True, help='Name of the database')
     parser.add_argument('-w', '--readwrite_role', required=True, help='Name of the readwrite role')
-    parser.add_argument('-u', '--user_role', required=True, help='Name of the user role')
+    parser.add_argument('-u', '--user', required=True, help='Name of the user')
     parser.add_argument('-p', '--user_pass', required=True, help='User password')
     parser.add_argument('-t', '--template', required=True, help='Path to the Jinja2 template file')
-    parser.add_argument('-o', '--output', required=True, help='Path to the output SQL script file')    
+    parser.add_argument('-o', '--output_file', required=True, help='Path of the output SQL script file')    
     args = parser.parse_args()
 
-    generate_sql_script(args.database, args.readwrite_role, args.user_role, args.user_pass, args.template, args.output)
+    class UserArgs:
+        def __init__(self, database, readwrite_role, user, user_pass, template, output_file):
+            self.database = database
+            self.readwrite_role = readwrite_role
+            self.user = user
+            self.user_pass = user_pass
+            self.template = template
+            self.output_file = output_file
+    
+    userargs = UserArgs(args.database, args.readwrite_role, args.user, args.user_pass, args.template, args.output_file)
+    
+    generate_sql_script(userargs)
 
 if __name__ == '__main__':
     main()
